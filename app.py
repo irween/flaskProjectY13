76cb675d5e321d370517579530a307db503e3e8a
Irwan Clark <19214@wc.school.nz>
2023-02-16 14:21:10 +1300

    Put css and other files into static and templates

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_page():
+    return render_template("home.html")
+
+
+@app.route('/menu')
+def menu_page():
+    return render_template("menu.html")
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
 
 
 if __name__ == '__main__':
